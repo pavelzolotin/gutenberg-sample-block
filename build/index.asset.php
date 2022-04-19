e9658df4853f7bb0913098889005fc7b80814876
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-19 16:04:57 +0500

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'b3dcb97ef6095d3bd75eeb12cc8c77c1');
---
+++ src/save.js
@@ -0,0 +1,10 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps } from '@wordpress/block-editor';
+
+export default function save() {
+	return (
+		<p { ...useBlockProps.save() }>
+			{ __( 'Hello from the saved new block!', 'sample-block' ) }
+		</p>
+	);
+}
